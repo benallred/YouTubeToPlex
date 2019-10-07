3b3bec373933f97370132b679379145c6dc677e2
Ben Allred <ben@bmallred.com>
2019-10-07 11:48:26 -0600

    Add DragonFruit and parameters

--- YouTubeToPlex/Program.cs
+++ YouTubeToPlex/Program.cs
@@ -4,7 +4,12 @@ namespace YouTubeToPlex
 {
 	class Program
 	{
-		static void Main(string[] args)
+		/// <summary>
+		/// Downloads videos from a YouTube playlist and creates metadata for use in media players.
+		/// </summary>
+		/// <param name="playlistId">The ID of the YouTube playlist.</param>
+		/// <param name="downloadFolder">The folder to download videos to.</param>
+		public static void Main(string playlistId, string downloadFolder)
 		{
 			Console.WriteLine("Hello World!");
 		}
