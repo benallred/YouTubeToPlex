8f227e74e8a67a3731e49433f9c94db26d50e4a0
Ben Allred <ben@bmallred.com>
2019-10-10 16:32:59 -0600

    Output file names with season/episode formatting

---
+++ YouTubeToPlex/ObjectExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace YouTubeToPlex
+{
+	internal static class ObjectExtensions
+	{
+		public static TTo Convert<TFrom, TTo>(this TFrom obj, Func<TFrom, TTo> convert)
+		{
+			return convert(obj);
+		}
+
+		public static TTo Convert<TFrom, TTo>(this TFrom? obj, Func<TFrom, TTo> some, Func<TTo> none)
+			where TFrom : class
+		{
+			return obj != null
+				? some(obj)
+				: none();
+		}
+
+		public static void Do<T>(this T? obj, Action<T> action, Action? none = null)
+			where T : class
+		{
+			if (obj != null)
+			{
+				action(obj);
+			}
+			else
+			{
+				none?.Invoke();
+			}
+		}
+	}
+}
