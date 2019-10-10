f10ab76eaebcf721c0b1482727b89ae2376e8f72
Ben Allred <ben@bmallred.com>
2019-10-09 23:10:29 -0600

    Download videos and captions

---
+++ YouTubeToPlex/ConcurrentProgress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace YouTubeToPlex
+{
+	internal class ConcurrentProgress<T> : IProgress<T>
+	{
+		private object Lock = new object();
+		private Action<T> Handler { get; }
+
+		public ConcurrentProgress(Action<T> handler)
+		{
+			Handler = handler;
+		}
+
+		public void Report(T value)
+		{
+			lock (Lock)
+			{
+				Handler(value);
+			}
+		}
+	}
+}
---
+++ YouTubeToPlex/NullableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YouTubeToPlex
+{
+	internal static class NullableExtensions
+	{
+		// I'm not sure if this is completely analogous, but I'm trying to simulate F#'s Option module:
+		// https://msdn.microsoft.com/visualfsharpdocs/conceptual/option.iter%5b%27t%5d-function-%5bfsharp%5d
+		public static void Iterate<T>(this T nullable, Action<T> action)
+			where T : class?
+		{
+			if (nullable != null)
+			{
+				action(nullable);
+			}
+		}
+	}
+}
